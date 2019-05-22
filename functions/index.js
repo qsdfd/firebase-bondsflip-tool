bf9d1ee384183a81a4cc2bc592108c719e9a3302
Kaisum <kaisumaro445@hotmail.com>
2019-05-22 23:10:42 +0200

    yjttjftjftjytfj

--- functions/index.js
+++ functions/index.js
@@ -1,12 +1,39 @@
 const functions = require('firebase-functions');
-const express = require('express');
+const axios = require('axios');
 
-const summary = require('./summary');
 
-const app = express();
+const BOND_ID = "13190";
+const OSBUDDY_GR_PRICES_SUMMARY_GOOGLE_APIS = 'https://storage.googleapis.com/osbuddy-exchange/summary.json';
+const OSRS_GE_API_URL = 'http://services.runescape.com/m=itemdb_oldschool/api/catalogue/detail.json?item=' + BOND_ID;
 
-app.get('/summary',  (request, response) => {
-    response.send(summary());
+
+exports.summary = functions.https.onRequest((req, res) => {
+    axios.get(OSBUDDY_GR_PRICES_SUMMARY_GOOGLE_APIS)
+    .then(summ => {
+        // let bond = summ.data[BOND_ID];
+        axios.get(OSRS_GE_API_URL)
+        .then(ge_api => {
+        //     let convert = Number(ge_api.data.item.current.price.slice(0, -1)) * 100000;
+        //     buy = bond.overall_average - (convert/2);
+        //     sell = bond.overall_average + (convert/2);
+        //     factor = Number((bond.buy_quantity / bond.sell_quantity).toFixed(2));
+
+        //     res.send({
+        //         calc : {
+        //             buy,
+        //             sell,
+        //             factor
+        //         },
+        //         bond,
+        //         convert
+            // });
+            res.send('ok')
+        })
+        .catch(err => {
+            res.send(err.message);
+        });
+    })
+    .catch(err => {
+        res.send(err.message);
+    });
 });
-
-exports.summary = functions.https.onRequest(app);
--- functions/summary.js
+++ functions/summary.js
@@ -3,9 +3,11 @@ const axios = require('axios');
 module.exports = () => {
     axios.get('https://storage.googleapis.com/osbuddy-exchange/summary.json')
     .then(res => {
+        console.log('eok')
         return "ok";
     })
     .catch(err => {
+        console.log('wok')
         return "nok";
     });
 }
