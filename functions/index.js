7b50c8d40506a04e3a4ad4523e31647bc1a63675
Kaisum <kaisumaro445@hotmail.com>
2019-05-23 20:19:04 +0200

    sgergvsvservserergvsrgsdgresrg

--- functions/index.js
+++ functions/index.js
@@ -1,39 +1,53 @@
 const functions = require('firebase-functions');
-const axios = require('axios');
-
 
 const BOND_ID = "13190";
-const OSBUDDY_GR_PRICES_SUMMARY_GOOGLE_APIS = 'https://storage.googleapis.com/osbuddy-exchange/summary.json';
-const OSRS_GE_API_URL = 'http://services.runescape.com/m=itemdb_oldschool/api/catalogue/detail.json?item=' + BOND_ID;
-
+const OSBUDDY_GR_PRICES_SUMMARY_GOOGLE_APIS_URL ="https://storage.googleapis.com/osbuddy-exchange/summary.json";
+const OSRS_GE_API_URL = `https://services.runescape.com/m=itemdb_oldschool/api/catalogue/detail.json?item=${BOND_ID}`;
+const FIREBASE_FIXED_VALS_URL = 'https://bondsflip-tool.firebaseio.com/vals.json';
 
 exports.summary = functions.https.onRequest((req, res) => {
-    axios.get(OSBUDDY_GR_PRICES_SUMMARY_GOOGLE_APIS)
-    .then(summ => {
-        // let bond = summ.data[BOND_ID];
-        axios.get(OSRS_GE_API_URL)
-        .then(ge_api => {
-        //     let convert = Number(ge_api.data.item.current.price.slice(0, -1)) * 100000;
-        //     buy = bond.overall_average - (convert/2);
-        //     sell = bond.overall_average + (convert/2);
-        //     factor = Number((bond.buy_quantity / bond.sell_quantity).toFixed(2));
+    axios
+        .all([
+            axios.get(OSBUDDY_GR_PRICES_SUMMARY_GOOGLE_APIS_URL),
+            axios.get(OSRS_GE_API_URL),
+            axios.get(FIREBASE_FIXED_VALS_URL)
+        ])
+        .then(axios.spread((summary, ge_api, fixed_vals) => {
+            let fixed = fixed_vals.data;
+            let bond = summary.data[BOND_ID];
+            let convert = Math.round(
+                Number(ge_api.data.item.current.price.slice(0, -1)) * 100000
+            );
+            let isReliable = bond && bond.overall_average && convert ? true : false;
 
-        //     res.send({
-        //         calc : {
-        //             buy,
-        //             sell,
-        //             factor
-        //         },
-        //         bond,
-        //         convert
-            // });
-            res.send('ok')
-        })
+            let floor = bond.overall_average - convert / 2;
+            let ceil = bond.overall_average + convert / 2;
+            let rest = bond.sell_quantity - bond.buy_quantity;
+            let factor = Number((bond.buy_quantity / bond.sell_quantity).toFixed(2));
+
+            let buy = floor - (floor % 100000) + 6000;
+            let sell = ceil - (ceil % 100000) + 94000;
+            let margin = sell - buy - convert;
+
+            res.send({
+                calc: {
+                    sug: {
+                        buy,
+                        sell,
+                        margin
+                    },
+                    floor,
+                    ceil,
+                    rest,
+                    factor,
+                    isReliable
+                },
+                convert,
+                bond,
+                fixed
+            });
+        }))
         .catch(err => {
             res.send(err.message);
         });
-    })
-    .catch(err => {
-        res.send(err.message);
-    });
 });
